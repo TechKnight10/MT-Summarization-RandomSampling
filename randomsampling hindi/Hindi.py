915a4da0967645af86852011f6d84397d912f8c6
Kalmit Kulkarni <89975943+TechKnight10@users.noreply.github.com>
2024-09-23 18:35:06 +0530

    Add files via upload

---
+++ randomsampling hindi/Hindi.py
@@ -0,0 +1,28 @@
+import random
+from datasets import load_dataset
+import pandas as pd
+
+dataset = load_dataset("csebuetnlp/xlsum", "hindi", split="test")
+
+random_indices = random.sample(range(len(dataset)), 150)
+
+
+sampled_data = dataset.select(random_indices)
+
+
+data = []
+for article in sampled_data:
+    data.append({
+        'URL Source': article['url'], 
+        'Article': article['text'],   
+        'Summary': article['summary']  
+    })
+
+
+df = pd.DataFrame(data)
+
+
+csv_file = "urls_and_articles_summary_hindi.csv"
+df.to_csv(csv_file, index=False, encoding='utf-8-sig')  
+
+print(f"CSV file saved: {csv_file}")
